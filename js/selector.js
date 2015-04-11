6a59bf40edc7cdd9d873748f4687cba40efb8883
Leslie Lin <leslie.s.lin@gmail.com>
2015-04-10 22:59:05 -0400

    JS fixed

--- js/selector.js
+++ js/selector.js
@@ -11,10 +11,10 @@ $(document).ready(function() {
         $.each(json, function(iterator, item) {     
             // get the img element from momadata.json file
              
-            console.log('AAaaa1',item.class[0].value);
-            console.log('AAaaa2',item.class[1].value);
-            console.log('AAaaa3',item.class[2].value);
-            console.log('AAaaa4',item.class[3].value);
+            // console.log('AAaaa1',item.class[0].value);
+            // console.log('AAaaa2',item.class[1].value);
+            // console.log('AAaaa3',item.class[2].value);
+            // console.log('AAaaa4',item.class[3].value);
             
             var li = $('<li style="display:inline-block">');
             var classVal = item.class[0].value + ' ' + item.class[1].value + ' ' + item.class[2].value + ' ' + item.class[3].value;
@@ -31,7 +31,6 @@ $(document).ready(function() {
                 .attr('MoMA_number', item['MoMA_Number'])
                 .appendTo(li)
                 .click(clickHandler)
-
             
                 console.log('Class.data -',classVal);
         });
@@ -41,22 +40,24 @@ $(document).ready(function() {
 
 
     $('.kicker').click(function() {
-        $('#Good_1 img').remove();
+        console.log('kicker');
         ableToClick = true;
+        $('.Good_1 img').remove();
+        $("#reset_button").addClass("kicker");
+        
+        console.log("true");
     });
+
 });
 
 function clickHandler(){
 
    if (ableToClick) {
 
-        $(".hidden").removeClass("hidden");
-        //remove class from CSS :  .hidden {display: none;}
-
         // assign ID to image
         // click img : THIS
         var url = $(this).attr('src');
-        var totalArray = $("#Good_1").children();
+        var totalArray = $(".Good_1").children();
         var numImages;
         for (var i = 0; i < totalArray.length; i++) {
             if (totalArray[i].src == url) {                               
@@ -66,18 +67,21 @@ function clickHandler(){
         +totalArray;
         console.log("totalArray:", totalArray);  
 
-            if (totalArray.length >= 4) {
+            if (totalArray.length >= 5) {
                 console.log("woof");
                 //$('img').off('click');
                 ableToClick = false;
-                alert("That's the last one");
+                // alert("That's the last one");
+                $(".kicker").removeClass("kicker");
+               
+        //remove class from CSS :  .hidden {display: none;}
             }
 
 
         var tempImg = document.createElement('img')
         tempImg.setAttribute('src', url)
-        // append img to div #Good_1
-        $('#Good_1').append(tempImg);
+        // append img to div .Good_1
+        $('.Good_1').append(tempImg);
     }
 
 }
